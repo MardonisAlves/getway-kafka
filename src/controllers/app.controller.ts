68146897439b5e84ee02a6c269f78a3f986a1c8e
Mardonis Alves Bezerra <mardonis.bezerra@gmail.com>
2022-09-22 15:24:48 -0300

    verificar usuario cadastrado no sistema

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,18 +1,38 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, OnModuleInit, Param, Post, Res } from '@nestjs/common';
 import { AppService } from '../services/app.service';
 import { UserDtos } from '../dtos/user.dtos';
-
+import { ClientKafka } from '@nestjs/microservices';
+import { Response } from 'express';
+import { lastValueFrom } from 'rxjs';
 @Controller('api/v1')
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+export class AppController implements OnModuleInit {
+  constructor(
+    private readonly appService: AppService,
+    @Inject("TEST_SERVICE") private readonly testSerrvice: ClientKafka) { }
 
   @Post('create/user')
-  createUser(@Body() createuser:UserDtos){
-    try {
-      return this.appService.createUser(createuser)
-    } catch (error) {
-      console.log(error);
-    }
+ async createUser(@Body() createuser: UserDtos, @Res() response:Response) {
+    const create = await this.appService.createUser(createuser);
+    return response.json(create)
   }
 
+  @Get('verificar/user/:email')
+  async verificarUser(
+    @Param('email') email: string,
+    @Res() response: Response) {
+    const verificarUser = this.testSerrvice.send('verificar_user', {
+      email: email
+    });
+    const getuser = await lastValueFrom(verificarUser);
+    return response.json(getuser);
+  }
+
+
+
+
+  async onModuleInit() {
+    this.testSerrvice.subscribeToResponseOf('verificar_user');
+    await this.testSerrvice.connect()
+  }
 }
+
--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,18 +1,26 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 import { UserDtos } from '../dtos/user.dtos';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit{
   constructor(@Inject("TEST_SERVICE") private readonly testSerrvice: ClientKafka){}
  
  async createUser(user:UserDtos){
   try {
-    return  this.testSerrvice.emit('create_user',user);
+    const createUser = this.testSerrvice.send('create_user',user);
+    const res = await lastValueFrom(createUser);
+    return res;
     
   } catch (error) {
     console.log(error);
     
   }
+  }
+
+  async onModuleInit() {
+    this.testSerrvice.subscribeToResponseOf('create_user');
+    await this.testSerrvice.connect()
   }
 }
