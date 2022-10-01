daa118a2aee349835ee0f3afcccc35b26201262f
Mardonis Alves Bezerra <mardonis.bezerra@gmail.com>
2022-10-01 18:26:38 -0300

    refatoração

--- src/dtos/user.dtos.ts
+++ src/dtos/user.dtos.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEAN, IsNotEmpty, IsString } from 'class-validator';
+import {  IsNotEmpty, IsString } from 'class-validator';
 
 export  class UserDtos{
     @ApiProperty({
